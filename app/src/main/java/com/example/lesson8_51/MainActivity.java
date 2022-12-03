21c5946950c025cf8bf081cc67caff7434b19af0
AdiletExp <turakulov_adilet@mail.ru>
2022-12-03 20:32:45 +0600

    add lifecycle

--- app/src/main/java/com/example/lesson8_51/MainActivity.java
+++ app/src/main/java/com/example/lesson8_51/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.lesson8_51;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
     }
 
     @Override
